e4c0c6752f96c0e700966f45de4cb8a4197e2b4a
clover73 <97086997+clover73@users.noreply.github.com>
2022-02-19 12:13:34 -0600

    Update and rename IModule.cs to ICloverPatch.cs

---
+++ Packages/RH.ModulePattern/Runtime/ICloverPatch.cs
@@ -0,0 +1,20 @@
+﻿//(C) RenderHeads PTY LTD 2021
+//Author: Ross Borchers
+
+namespace RenderHeads.Tooling.Core.CloverPatchPattern
+{
+	/// <summary>
+	/// Base interface for all cloverpatchs, ICloverPatchFactory requires cloverpatchs inherit from this.
+	/// General cloverpatch usage pattern is:
+	/// RAII. All initialization is done through the constructor. After a constructor is run the cloverpatch should be fully usable.
+	/// UpdateCloverPatchs should be called at regular intervals. The order of cloverpatch updates should be controlled by the caller to ensure all dependencies have already been updated.
+	/// Pass in dependencies through the constructor IOC like.
+	/// </summary>
+	public interface ICloverPatch
+	{
+		/// <summary>
+		/// Should be called at a regular interval. The order of cloverpatch updates should be controlled by the caller to ensure all dependencies have already been updated.
+		/// </summary>
+		void UpdateCloverPatch();
+	}
+}
